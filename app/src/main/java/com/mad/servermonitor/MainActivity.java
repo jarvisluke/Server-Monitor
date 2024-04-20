a1cb55b6591510d75535e71a19ab9dba63dea7c7
noah0827 <167452647+noah0827@users.noreply.github.com>
2024-04-20 18:22:40 -0400

    Update MainActivity.java

--- app/src/main/java/com/mad/servermonitor/MainActivity.java
+++ app/src/main/java/com/mad/servermonitor/MainActivity.java
@@ -21,4 +21,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+    if (navHostFragment != null) {
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
 }
